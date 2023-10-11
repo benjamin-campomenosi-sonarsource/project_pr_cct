60046d6e7b333c67585b004c1ca0de4389304fe9
Benjamin Campomenosi <benjamin.campomenosi@sonarsource.com>
2023-10-11 14:10:36 +0200

    commit with issue

--- src/file1.js
+++ src/file1.js
@@ -2,8 +2,19 @@ let a = "coucou";
 
 console.log(a);
 
-if (a == 'howdy') {
-    console.log("coucou");
-} 
+if (a == "howdy") {
+  console.log("coucou");
+}
 
-var toto = 'ooo';
+var toto = "ooo";
+
+function concatenate() {
+  let args = Array.prototype.slice.call(arguments); // Noncompliant
+  return args.join(", ");
+}
+
+const x = 42;
+await x; // Noncompliant: x is a number, not a promise
+
+const pi = 3.14;
+pi = 3.14159;
